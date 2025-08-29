fd8123a420acc4dcc0135ef41cc403b1a7069e8a
Aaryan Kumar <112887779+9aaryanCyberH@users.noreply.github.com>
2025-08-29 21:59:58 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,24 @@
+const busRoutes = [
+  {
+    number: "215A",
+    stops: ["Dumdum", "Ultadanga", "Sealdah", "Esplanade"],
+    timings: ["7:00 AM", "8:00 AM", "9:30 AM", "11:00 AM"],
+    coordinates: [
+      [22.6276, 88.4220], // Dumdum
+      [22.5996, 88.4016], // Ultadanga
+      [22.5667, 88.3700], // Sealdah
+      [22.5665, 88.3476]  // Esplanade
+    ]
+  },
+  {
+    number: "223",
+    stops: ["Barasat", "Dumdum", "Shyambazar", "Howrah"],
+    timings: ["6:30 AM", "8:00 AM", "10:00 AM", "12:00 PM"],
+    coordinates: [
+      [22.7260, 88.4753], // Barasat
+      [22.6276, 88.4220], // Dumdum
+      [22.6000, 88.3700], // Shyambazar
+      [22.5850, 88.3468]  // Howrah
+    ]
+  }
+];
